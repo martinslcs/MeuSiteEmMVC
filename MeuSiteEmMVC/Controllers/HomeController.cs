5a32e32069cebbaa5c9a11b90e860cafe0029f20
matinslcs <ccplucasmartins@gmail.com>
2025-01-05 14:57:07 -0300

    Adicionar arquivos de projeto.

---
+++ MeuSiteEmMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using MeuSiteEmMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuSiteEmMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Lucas Martins";
+
+            home.Email = "martinslucasds@hotmail.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
